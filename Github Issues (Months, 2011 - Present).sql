605b398982da74f6b20a558ba66737c6b4d499a6
Jacob Malland <62392847+JMalland@users.noreply.github.com>
2025-04-07 21:47:18 -0500

    Update Github Issues (Months, 2011 - Present).sql
    Fixed improper counting of issues.

--- Github Issues (Months, 2011 - Present).sql
+++ Github Issues (Months, 2011 - Present).sql
@@ -1,36 +1,47 @@
-WITH github_issues AS (
+WITH gh_lang AS (
   SELECT
-    COALESCE(ght.language, gh_language.name) AS language,
+    gh_language.name AS language,
     EXTRACT(YEAR FROM PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', JSON_EXTRACT_SCALAR(payload, '$.issue.created_at'))) AS year,
     EXTRACT(MONTH FROM PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', JSON_EXTRACT_SCALAR(payload, '$.issue.created_at'))) AS month,
-    COUNT(DISTINCT events.id) AS issues,
+    JSON_EXTRACT_SCALAR(events.payload, '$.issue.id') AS issue_id
   FROM
     `githubarchive.year.20*` AS events
-  JOIN -- Find the repo's language with public github data
+  JOIN
     `bigquery-public-data.github_repos.languages` AS gh
-    ON
-      events.repo.name = gh.repo_name,
-      UNNEST(gh.language) AS gh_language
-  LEFT OUTER JOIN -- Resolve not-found repos ghtorrent
-    `ghtorrent-bq.ght.project_languages` AS ght
-    ON
-      events.repo.id = ght.project_id
+    ON events.repo.name = gh.repo_name,
+    UNNEST(gh.language) AS gh_language
   WHERE
-    events.type = 'IssuesEvent' -- Only get Issues for this table
-    AND
-    JSON_EXTRACT_SCALAR(events.payload, '$.action') = 'opened' -- Only select Issues that were Opened
-  GROUP BY
-    language, year, month
+    events.type = 'IssuesEvent' AND
+    JSON_EXTRACT_SCALAR(events.payload, '$.action') = 'opened'
+  GROUP BY language, year, month, issue_id
+),
+ght_lang AS (
+  SELECT
+    ght.language AS language,
+    EXTRACT(YEAR FROM PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', JSON_EXTRACT_SCALAR(payload, '$.issue.created_at'))) AS year,
+    EXTRACT(MONTH FROM PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', JSON_EXTRACT_SCALAR(payload, '$.issue.created_at'))) AS month,
+    JSON_EXTRACT_SCALAR(events.payload, '$.issue.id') AS issue_id
+  FROM
+    `githubarchive.year.20*` AS events
+  JOIN
+    `ghtorrent-bq.ght.project_languages` AS ght
+    ON events.repo.id = ght.project_id
+  WHERE
+    events.type = 'IssuesEvent' AND
+    JSON_EXTRACT_SCALAR(events.payload, '$.action') = 'opened'
+  GROUP BY language, year, month, issue_id
+),
+combined_issues AS (
+  SELECT * FROM gh_lang
+  UNION DISTINCT
+  SELECT * FROM ght_lang
 )
 
 SELECT
   language,
   year,
   month,
-  issues
-FROM 
-  github_issues
-GROUP BY
-  language, year, month, issues
-ORDER BY
-  year DESC, month DESC, issues DESC
+  COUNT(DISTINCT issue_id) AS issues
+FROM combined_issues
+GROUP BY language, year, month
+ORDER BY year DESC, month DESC, issues DESC;
